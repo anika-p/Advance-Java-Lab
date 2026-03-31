aa0be02c82bda14e5aa4157206db2362d779832c
anika-p <anika01032003@gmail.com>
2026-03-31 09:56:37 +0530

    more practice ques added

--- unit3/EmployeeQSet.java
+++ unit3/EmployeeQSet.java
@@ -64,5 +64,113 @@ public class EmployeeQSet {
                .ifPresent(e ->
                    System.out.println("Oldest Employee: " + e.getName() + " - " + e.getAge())
                );
+
+        //Q9
+        Set<String> seen = new HashSet<>();
+
+        List<Employee> unique = empList.stream()
+                              .filter(e -> seen.add(e.getName()))
+                              .toList();
+
+        Set<String> seen1 = new HashSet<>();
+
+        empList.stream()
+            .filter(e -> seen1.add(e.getName()))
+            .forEach(e -> System.out.println(e.getName()));
+
+        //Q13
+        empList.stream()
+       .filter(e -> e.getAge() > 25)
+       .min(Comparator.comparingDouble(Employee::getSalary))
+       .ifPresent(e -> System.out.println(e.getName() + " - " + e.getSalary()));
+
+
+       //Q15
+       empList.stream()
+       .limit(5)
+       .forEach(e -> System.out.println(e.getName()));
+
+       //Q16
+       empList.stream()
+       .skip(3)
+       .forEach(e -> System.out.println(e.getName()));
+
+       //Q3
+       List<Integer> list = Arrays.asList(25,10,5,30,15,20);
+
+        int sum = list.stream()
+                      .filter(x -> x % 2 == 0 && x > 10)
+                      .map(x -> x * x)
+                      .reduce(0, Integer::sum);
+
+        System.out.println(sum);
+        
+        //Q4
+        list.stream()
+            .filter(x -> x % 2 == 0)
+            .max(Integer::compare)
+            .ifPresent(System.out::println);
+
+
+        //Q5
+        list.stream()
+            .sorted(Comparator.reverseOrder()) // descending
+            .skip(2)                           // remove top 2 largest
+            .forEach(System.out::println);
+
+        //Q6
+        list.stream()
+            .map(x -> x * 3)
+            .min(Integer::compare)
+            .ifPresent(System.out::println);
+
+        //Q7
+        int sum1 = list.stream()
+                      .filter(x -> x >= 10 && x <= 40)
+                      .reduce(0, Integer::sum);
+
+        System.out.println(sum1);
+
+        //Q8
+        list.stream()
+            .sorted(Comparator.reverseOrder())
+            .skip(2)
+            .findFirst()
+            .ifPresent(System.out::println);
+
+        //Q9
+        list.stream()
+            .map(x -> x * x)
+            .sorted(Comparator.reverseOrder())
+            .limit(4)
+            .forEach(System.out::println);
+
+        //Q10
+        int sum2= list.stream()
+                      .sorted()
+                      .limit(4)
+                      .reduce(0, Integer::sum);
+
+        System.out.println(sum2);
+
+        //Q11
+        list.stream()
+            .sorted()        // ascending
+            .skip(3)         // skip 3 smallest
+            .max(Integer::compare)
+            .ifPresent(System.out::println);
+
+        //Q12
+        list.stream()
+            .filter(x -> x % 2 != 0 && x > 20)
+            .min(Integer::compare)
+            .ifPresent(System.out::println);
+
+        //Q13
+        int product = list.stream()
+                          .filter(x -> x > 5)
+                          .reduce(1, (a, b) -> a * b);
+
+        System.out.println(product);
     }
 }
